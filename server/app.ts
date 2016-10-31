0ade7dfefcc3afa179df5b520a147543877ec0c3
vlad33 <nirzol@gmail.com>
2016-10-31 16:06:09 +0200

    Adding express server

---
+++ server/app.ts
@@ -0,0 +1,16 @@
+import * as express from "express";
+import * as path from "path";
+
+const app = express();
+const basePath = path.join(process.cwd(), "..");
+const port = 8001;
+
+console.log("Running express server");
+console.log("    basePath: " + basePath);
+console.log("    port: " + port);
+
+app.use(express.static(basePath));
+
+app.listen(port, function () {
+    console.log('App listening');
+});
