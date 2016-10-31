9783be32e2f504234776043e9832c8c4d7b5f1c0
ocalvo <ori.calvo@gmail.com>
2016-10-31 14:47:25 +0200

    Initial

---
+++ app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'my-app',
+    template: require("./app.component.html!text"),
+    styles: [require("./app.component.css!text")]
+})
+export class AppComponent { }
